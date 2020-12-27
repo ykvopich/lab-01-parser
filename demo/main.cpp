46ee0ebf306ffc192846e2f728b24e22968eb2e5
ykvopich <kravchenko_vlad2001@bk.ru>
2020-12-27 06:39:22 +0300

    yamashi

---
+++ demo/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <students.hpp>
+int main(){
+    TEST(StudentsTest, Parse){
+        const char jsonString[] = R"({
+        "items": [
+        {
+        "name": "Ivanov Petr",
+        "group": "1",
+        "avg": "4.25",
+        "debt": null
+        },
+        {
+        "name": "Sidorov Ivan",
+        "group": 31,
+        "avg": 4,
+        "debt": "C++"
+        },
+        {
+        "name": "Pertov Nikita",
+        "group": "IU8-31",
+        "avg": 3.33,
+        "debt": [
+        "C++",
+        "Linux",
+        "Network"
+        ]
+        }
+        ],
+        "_meta": {
+        "count": 3
+        }
+        })";
+        std::ofstream jsonFile("jsonFile.json", std::ios::out);
+        jsonFile << jsonString;
+        jsonFile.close();
+
+        std::cout << Students::ParseFile("jsonFile.json");
+    }
+};
